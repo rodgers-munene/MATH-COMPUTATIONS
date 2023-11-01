76924ffefa72102d2950583fee66b53dbd92a9d5
victorwanyeki <victorwanyeki@windows.net>
2023-11-01 10:03:04 +0300

    Created an instance of Division Class in the Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        // Create an instance of the Division Class
+        Division divisionInstance = new Division();
+
+        // get user input by creating a Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt for input
+        System.out.println("Enter the first number: ");
+        double firstNumber = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double secondNumber = scanner.nextDouble();
+
+        // call the divide method from Division class
+        double resultVal = divisionInstance.divide(firstNumber, secondNumber);
+
+        // Display result to user
+        System.out.println("Result: " + resultVal);
+        
+        // close the scanner (java best practice)
+        scanner.close();
     }
 }
